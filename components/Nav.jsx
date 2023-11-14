06087a5d422483cc8cffc31a473d4b4aedbe4fc2
Arkopravo Das <arko.andrew2000@gmail.com>
2023-11-15 03:07:28 +0530

    others profile view seventh commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,12 +12,10 @@ const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    }
-    setUpProviders();
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
   }, []);
 
   return (
@@ -84,7 +82,7 @@ const Nav = () => {
               height={37}
               className='rounded-full'
               alt='profile'
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
